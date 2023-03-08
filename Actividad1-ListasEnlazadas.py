2c1f57f5dcb4cf269ea19c670de7909d7f414e02
jgayon <jgayon@uninorte.edu.co>
2023-03-08 16:20:18 -0500

    modificacion

--- Actividad1-ListasEnlazadas.py
+++ Actividad1-ListasEnlazadas.py
@@ -1,23 +1,61 @@
-class Nodo:
-    def __init__(self, data = None, next=None) -> None:
+# Creamos la clase node
+class node:
+    def __init__(self, data = None, next = None):
         self.data = data
         self.next = next
 
-class ListaEnlazada:
-    def __init__(self) -> None:
+# Creamos la clase linked_list
+class linked_list: 
+    def __init__(self):
         self.head = None
     
-    def insert (self, new_node):
-        if self.head:
-			## getting the last node
-			last_node = self.head
-			while last_node != None:
-				last_node = last_node.next
+    # Método para agregar elementos en el frente de la linked list
+    def add_at_front(self, data):
+        self.head = node(data=data, next=self.head)  
 
-			## assigning the new node to the next pointer of last node
-			last_node.next = new_node
+    # Método para verificar si la estructura de datos esta vacia
+    def is_empty(self):
+        return self.head == None
 
-		else:
-			## head is empty
-			## assigning the node to head
-			self.head = new_node
+    # Método para agregar elementos al final de la linked list
+    def add_at_end(self, data):
+        if not self.head:
+            self.head = node(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = node(data=data)
+    
+    # Método para eleminar nodos
+    def delete_node(self, key):
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+
+    # Método para obtener el ultimo nodo
+    def get_last_node(self):
+        temp = self.head
+        while(temp.next is not None):
+            temp = temp.next
+        return temp.data
+
+    # Método para imprimir la lista de nodos
+    def print_list( self ):
+        node = self.head
+        while node != None:
+            print(node.data, end =" => ")
+            node = node.next
+
+s = linked_list()
+s.add_at_front(1)
+s.add_at_end(2)
+s.add_at_end(3)
+s.print_list()
