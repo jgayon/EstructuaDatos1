ec40a7953d7f295c8cfa06a1dd1f67cbeb441452
Carlosdm19 <99750007+Carlosdm19@users.noreply.github.com>
2023-03-22 18:47:57 -0500

    Update Punto2.py

--- Actividad 2/Punto2.py
+++ Actividad 2/Punto2.py
@@ -1,1 +1,12 @@
 from listas import linked_list
+def count_unique_elements(linked_list):
+    unique_elements = set()
+    current_node = linked_list.head
+    while current_node is not None:
+        unique_elements.add(current_node.value)
+        current_node = current_node.next
+    return len(unique_elements)
+
+num_unique_elements = count_unique_elements(linked_list)
+print("El número total de elementos diferentes en la lista enlazada es:", num_unique_elements)
+
